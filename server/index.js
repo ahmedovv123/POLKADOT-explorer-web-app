8982fc76eca48edec75f7c0f2d310572be413dcf
Bojidar00 <bojidar_rusalinov@abv.bg>
2021-09-09 13:14:58 +0300

    Added Dockerfile

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var serveStatic = require('serve-static');
-var path ="../dist";
+var path ="/home/dist";
 app.use(serveStatic(path));
 
 app.get('/', function (req,res) {
